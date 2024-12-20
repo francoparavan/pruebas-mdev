7e420e7ff3f946a9abe726ba693e38064a3121a9
Franco Paravan <francoparavan@gmail.com>
2024-12-20 18:41:52 +1100

    quinto commit

---
+++ projects/mouse-follower/src/App.jsx
@@ -0,0 +1,35 @@
+import { useEffect, useState } from "react"
+
+function App() {
+
+  const [enabled, setEnabled] = useState(false)
+
+  useEffect(() => {
+    console.log('Effect', { enabled })
+  }, [enabled])
+
+  const changeMouseStatus = () => setEnabled(!enabled)
+
+  return (
+    <main>
+      <div style={{
+        position: 'absolute',
+        backgroundColor: '#09f',
+        borderRadius: '50%',
+        opacity: 0.8,
+        pointerEvents: 'none',
+        left: -20,
+        top: -20,
+        width: 40,
+        height: 40,
+        transform: 'translate(0px, 0px)'
+      }}
+      />
+      <button onClick={changeMouseStatus}>
+        {enabled ? 'Desactivar' : 'Desactivar'} seguir puntero
+      </button>
+    </main>
+  )
+}
+
+export default App
